cf68d61009098c42e6878791bb16aeb0e255fba4
Aditya Shakya <aditya.shakya@auto-grid.com.com>
2019-08-29 03:05:33 +0530

    showing all families

--- app/controllers/universe_controller.rb
+++ app/controllers/universe_controller.rb
@@ -1,2 +1,7 @@
 class UniverseController < ApplicationController
+
+  def families
+    @families = Family.joins(:people).where(people: {universe_id: params[:id]})
+    render json: @families
+  end
 end
